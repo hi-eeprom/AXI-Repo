9388e5b7729ea46494c30657ccc4120b559ee029
jomonkjoy <jomonkurianjoy@gmail.com>
2017-11-12 09:05:23 +0000

    Update m_axi_mem.sv

--- m_axi_mem.sv
+++ m_axi_mem.sv
@@ -14,49 +14,49 @@ module m_axi_mem #(
   output logic txn_done,
   output logic txn_error,
   
-  input  logic m_axi_aclk,
-  input  logic m_axi_aresetn,
-  output logic [ID_WIDTH-1:0] m_axi_awid,
-  output logic [ADDR_WIDTH-1:0] m_axi_awaddr,
-  output logic [7:0] m_axi_awlen,
-  output logic [2:0] m_axi_awsize,
-  output logic [1:0] m_axi_awburst,
-  output logic m_axi_awlock,
-  output logic [3:0] m_axi_awcache,
-  output logic [2:0] m_axi_awport,
-  output logic [3:0] m_axi_awqos,
+  input  logic                    m_axi_aclk,
+  input  logic                    m_axi_aresetn,
+  output logic [ID_WIDTH-1:0]     m_axi_awid,
+  output logic [ADDR_WIDTH-1:0]   m_axi_awaddr,
+  output logic [7:0]              m_axi_awlen,
+  output logic [2:0]              m_axi_awsize,
+  output logic [1:0]              m_axi_awburst,
+  output logic                    m_axi_awlock,
+  output logic [3:0]              m_axi_awcache,
+  output logic [2:0]              m_axi_awport,
+  output logic [3:0]              m_axi_awqos,
   output logic [AWUSER_WIDTH-1:0] m_axi_awuser,
-  output logic m_axi_awvalid,
-  input  logic m_axi_awready,
-  output logic [DATA_WIDTH-1:0] m_axi_wdata,
+  output logic                    m_axi_awvalid,
+  input  logic                    m_axi_awready,
+  output logic [DATA_WIDTH-1:0]   m_axi_wdata,
   output logic [DATA_WIDTH/8-1:0] m_axi_wstrb,
-  output logic m_axi_wlast,
-  output logic [WUSER_WIDTH-1:0] m_axi_wuser,
-  output logic m_axi_wvalid,
-  input  logic m_axi_wready,
-  input  logic [ID_WIDTH-1:0] m_axi_bid,
-  input  logic [1:0] m_axi_bresp,
-  input  logic [BUSER_WIDTH-1:0] m_axi_buser,
-  input  logic m_axi_wvalid,
-  output logic m_axi_wready,
-  output logic [ID_WIDTH-1:0] m_axi_arid,
-  output logic [ADDR_WIDTH-1:0] m_axi_araddr,
-  output logic [7:0] m_axi_arlen,
-  output logic [2:0] m_axi_arsize,
-  output logic [1:0] m_axi_arburst,
-  output logic m_axi_arlock,
-  output logic [3:0] m_axi_arcache,
-  output logic [2:0] m_axi_arport,
-  output logic [3:0] m_axi_arqos,
+  output logic                    m_axi_wlast,
+  output logic [WUSER_WIDTH-1:0]  m_axi_wuser,
+  output logic                    m_axi_wvalid,
+  input  logic                    m_axi_wready,
+  input  logic [ID_WIDTH-1:0]     m_axi_bid,
+  input  logic [1:0]              m_axi_bresp,
+  input  logic [BUSER_WIDTH-1:0]  m_axi_buser,
+  input  logic                    m_axi_wvalid,
+  output logic                    m_axi_wready,
+  output logic [ID_WIDTH-1:0]     m_axi_arid,
+  output logic [ADDR_WIDTH-1:0]   m_axi_araddr,
+  output logic [7:0]              m_axi_arlen,
+  output logic [2:0]              m_axi_arsize,
+  output logic [1:0]              m_axi_arburst,
+  output logic                    m_axi_arlock,
+  output logic [3:0]              m_axi_arcache,
+  output logic [2:0]              m_axi_arport,
+  output logic [3:0]              m_axi_arqos,
   output logic [ARUSER_WIDTH-1:0] m_axi_aruser,
-  output logic m_axi_arvalid,
-  input  logic m_axi_arready,
-  input  logic [ID_WIDTH-1:0] m_axi_rid,
-  input  logic [DATA_WIDTH-1:0] m_axi_rdata,
-  input  logic [1:0] m_axi_rresp,
-  input  logic [RUSER_WIDTH-1:0] m_axi_ruser,
-  input  logic m_axi_rvalid,
-  output logic m_axi_rready
+  output logic                    m_axi_arvalid,
+  input  logic                    m_axi_arready,
+  input  logic [ID_WIDTH-1:0]     m_axi_rid,
+  input  logic [DATA_WIDTH-1:0]   m_axi_rdata,
+  input  logic [1:0]              m_axi_rresp,
+  input  logic [RUSER_WIDTH-1:0]  m_axi_ruser,
+  input  logic                    m_axi_rvalid,
+  output logic                    m_axi_rready
 );
   
   logic m_axi_areset;
@@ -91,35 +91,35 @@ module m_axi_mem #(
   logic wr_resp_error;
   logic rd_resp_error;
   
-  assign m_axi_awid = 'b0;
-  assign m_axi_awaddr = axi_awaddr;
-  assign m_axi_awlen = BURST_LEN-1;
-  assign m_axi_awsize = $clog2(DATA_WIDTH/8-1);
+  assign m_axi_awid    = 'b0;
+  assign m_axi_awaddr  = axi_awaddr;
+  assign m_axi_awlen   = BURST_LEN-1;
+  assign m_axi_awsize  = $clog2(DATA_WIDTH/8-1);
   assign m_axi_awburst = 2'b01;
-  assign m_axi_awlock = 1'b0;
+  assign m_axi_awlock  = 1'b0;
   assign m_axi_awcache = 4'b0010;
-  assign m_axi_awport = 3'h0;
-  assign m_axi_awqos = 4'h0;
-  assign m_axi_awuser = 'b1;
+  assign m_axi_awport  = 3'h0;
+  assign m_axi_awqos   = 4'h0;
+  assign m_axi_awuser  = 'b1;
   assign m_axi_awvalid = axi_awvalid;
-  assign m_axi_wdata = axi_wdata;
-  assign m_axi_wstrb = {DATA_WIDTH/8{1'b1}};
-  assign m_axi_wlast = axi_wlast;
-  assign m_axi_wuser = 'b0;
-  assign m_axi_wvalid = axi_wvalid;
-  assign m_axi_bready = axi_bready;
-  assign m_axi_arid = 'h0;
-  assign m_axi_araddr = axi_araddr;
-  assign m_axi_arlen = BURST_LEN-1;
-  assign m_axi_arsize = $clog2(DATA_WIDTH/8-1);
+  assign m_axi_wdata   = axi_wdata;
+  assign m_axi_wstrb   = {DATA_WIDTH/8{1'b1}};
+  assign m_axi_wlast   = axi_wlast;
+  assign m_axi_wuser   = 'b0;
+  assign m_axi_wvalid  = axi_wvalid;
+  assign m_axi_bready  = axi_bready;
+  assign m_axi_arid    = 'h0;
+  assign m_axi_araddr  = axi_araddr;
+  assign m_axi_arlen   = BURST_LEN-1;
+  assign m_axi_arsize  = $clog2(DATA_WIDTH/8-1);
   assign m_axi_arburst = 2'b01;
-  assign m_axi_arlock = 1'b0;
+  assign m_axi_arlock  = 1'b0;
   assign m_axi_arcache = 4'b0010;
   assign m_axi_arwport = 3'h0;
-  assign m_axi_arqos = 4'h0;
-  assign m_axi_aruser = 'b1;
+  assign m_axi_arqos   = 4'h0;
+  assign m_axi_aruser  = 'b1;
   assign m_axi_arvalid = axi_arvalid;
-  assign m_axi_rready = axi_rready;
+  assign m_axi_rready  = axi_rready;
   
   assign txn_done = wr_done | rd_done;
   
